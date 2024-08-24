95dd94e1a5c20a4268239f809b91e840f8c99d67
Lucas Manueel <testadormoz@gmail.com>
2024-08-24 23:35:34 +0200

    Formatei o codigo deste arquivo

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,7 @@ exports.createUser = (req, res) => {
   console.log('Dados recebidos:', { firstname, lastname, email, password });
 
   User.findOne({ where: { email: email } })
+
     .then(user => {
       if (user) {
         console.log('Usuário já existe:', email);
@@ -14,10 +15,10 @@ exports.createUser = (req, res) => {
 
       return bcrypt.hash(password, 10);
     })
+
     .then(hashedPassword => {
       if (!hashedPassword) throw new Error('Falha ao gerar o hash da senha');
       
-      console.log('Criando usuário com dados:', { firstname, lastname, email });
       return User.create({
         firstname,
         lastname,
@@ -25,10 +26,12 @@ exports.createUser = (req, res) => {
         password: hashedPassword,
       });
     })
+
     .then(() => {
       console.log('Usuário criado com sucesso:', email);
       return res.status(201).json({ msg: 'Usuário criado com sucesso' });
     })
+    
     .catch(error => {
       console.error('Erro ao criar o usuário:', error.message);
       return res.status(500).json({ error: 'Erro ao criar o usuário', details: error.message });
