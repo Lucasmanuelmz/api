6f7c9dd14e7f50295e73d4ead3d125070aa313f7
Lucas Manueel <testadormoz@gmail.com>
2024-08-24 22:08:24 +0200

    Adicinei UserId chave estrangeira para monitoriar o usuario que adicionou a categoria

--- router/categoryRouter.js
+++ router/categoryRouter.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const categoryController = require('../controllers/categoryController')
 const categoryRouter = Router();
+const passport = require('../config/passport');
 
-categoryRouter.post('/create', categoryController.createCategory)
+categoryRouter.post('/create', passport.authenticate('jwt', 
+  {session: false}) , categoryController.createCategory);
 
 module.exports = categoryRouter;
