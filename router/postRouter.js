54755a66fc47f300fa5eba24a675ab90bf7c4a8f
Lucas Manueel <testadormoz@gmail.com>
2024-08-24 22:11:44 +0200

    Tambem adicionei o middleware de authenicacao a rota category e post e tudo esta funcionando corretamente. O proximo passo e adicionar rota de comentarios e outras rotas do CRUD

--- router/postRouter.js
+++ router/postRouter.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const postController = require('../controllers/postController')
 const postRouter = Router();
+const passport = require('../config/passport');
 
-postRouter.post('/create', postController.createPost);
+postRouter.post('/create', passport.authenticate('jwt', 
+  {session: false}), postController.createPost);
 
 module.exports = postRouter;
