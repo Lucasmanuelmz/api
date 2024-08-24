ff6bbdfdc90511440895583db7c80a3ee34a0ee6
Lucas Manueel <testadormoz@gmail.com>
2024-08-24 17:37:32 +0200

    Adicionei tres arquivos do controller e os configurei n app.js. Estao funcionando de forma devida

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const express = require('express');
-const app = express.Router();
+const app = express();
+
+const userRouter = require('./router/userRouter');
+const postRouter = require('./router/postRouter');
+const categoryRouter = require('./router/categoryRouter');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Correto');
+});
+
+app.use('/users', userRouter);       
+app.use('/posts', postRouter);       
+app.use('/categories', categoryRouter); 
 
 module.exports = app;
