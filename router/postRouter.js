b05851eccef2dca95f84a068412b6ab7ce5e58a9
Lucas Manueel <testadormoz@gmail.com>
2024-08-25 07:05:36 +0200

    Adicionei uma rota get neste modulo

--- router/postRouter.js
+++ router/postRouter.js
@@ -6,4 +6,6 @@ const passport = require('../config/passport');
 postRouter.post('/create', passport.authenticate('jwt', 
   {session: false}), postController.createPost);
 
+postRouter.get('/get', postController.getPosts);
+
 module.exports = postRouter;
