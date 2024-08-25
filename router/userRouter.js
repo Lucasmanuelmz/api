4ccfde9a1abf138f880b7f35a4beb840980e3eb9
Lucas Manueel <testadormoz@gmail.com>
2024-08-25 07:07:17 +0200

    Adicionei uma rota get neste modulo, Tudo o que fiz ate aqui esta funcionando corretamente sem erros, tanto para cadastrar como para obter

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const userController = require('../controllers/userController')
 const userRouter = Router();
+const passport = require('../config/passport')
 
 userRouter.post('/create', userController.createUser)
+userRouter.get('/get', passport.authenticate('jwt', 
+  {session: false}), userController.getUsers);
 
 module.exports = userRouter;
