5409771dd610357c40c31fc89083f9acc6cd821b
Lucas Manueel <testadormoz@gmail.com>
2024-08-25 06:55:53 +0200

    Adicionei uma duncao get neste controller para obter todos os comentarios de cada post

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,4 +1,6 @@
 const Comment = require('../models/commentModel');
+const Post = require('../models/postModel');
+
 
 exports.createComments = (req, res) => {
   const { comment, postId, userId } = req.body;
@@ -15,5 +17,36 @@ exports.createComments = (req, res) => {
 
   .catch(error => {
    res.status(500).json({msg: 'O comentario nao foi criado', details: error.message})
+  })
+};
+
+exports.getComments = (req, res) => {
+  const id = req.params.id
+  Comment.findByPk(id)
+
+  .then(post=> {
+
+    if(!post) { 
+      return res.status(404).json({msg: 'Este artigo ainda nao tem comentarios, seja o primeiro a comentar'})
+    };
+
+    Comment.findAll()
+
+    .then(comments => {
+
+      if(!comments) { 
+        return res.status(404).json({msg: 'Este artigo ainda nao tem comentarios, seja o primeiro a comentar'})
+      };
+
+      return res.status(200).json({comments});
+    }) 
+
+    .catch (error => {
+      return res.status(500).json({error: 'Erro ao obter comentarios', details: error.message})
+    })
+  })
+
+  .catch (error => {
+    return res.status(500).json({error: 'Erro ao obter post', details: error.message})
   })
 }
