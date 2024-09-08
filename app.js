286812a26610a301a62f2f6a7f720b21bf4b91b6
Lucas Manueel <testadormoz@gmail.com>
2024-09-08 07:33:44 +0200

    Configurei o helmet e mudei a variavel de ambiente para testar se tudo esta funcionando corretamente no backend. mas at agora tudo esta bem.

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const helmet = require('helmet');
-require('dotenv').config()
-const baseUrl = process.env.BASE_URL || 'http://localhost:3000'
 
 const userRouter = require('./router/userRouter');
 const postRouter = require('./router/postRouter');
@@ -15,17 +13,16 @@ const currentUser = require('./middlewares/currentUser');
 const passport = require('./config/passport');
 const sequelize = require('./models/syncronization');
 
-app.use(helmet()); 
+app.use(helmet({
+  crossOriginResourcePolicy: { policy: "cross-origin" }, 
+  crossOriginOpenerPolicy: { policy: "same-origin" }  
+}));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); 
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
 
 
-app.use(cors({
-  origin: baseUrl, 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 
 app.get('/', (req, res) => {
@@ -35,10 +32,10 @@ app.get('/', (req, res) => {
 app.use('/api/current-user', passport.authenticate('jwt', { session: false }), currentUser.currentUser);
 
 app.use('/api', authRouter);
-app.use('/api/users', userRouter);       
-app.use('/api/posts', postRouter);
-app.use('/api/posts/:id/comments', commentRouter);
-app.use('/api/categories', categoryRouter); 
+app.use('/api', userRouter);       
+app.use('/api', postRouter);
+app.use('/api/posts/:id', commentRouter);
+app.use('/api', categoryRouter); 
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
